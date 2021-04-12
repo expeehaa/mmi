2cbb41f7c9583fb38991962e22d73be44db00b25
expeehaa <expeehaa@outlook.com>
2021-04-12 13:31:35 +0200

    Create source processor instances on initialize.

--- lib/mmi/assets_processor.rb
+++ lib/mmi/assets_processor.rb
@@ -11,32 +11,30 @@ module Mmi
 			assets ||= []
 			
 			if assets.is_a?(Array)
-				@assets = assets
+				@assets = assets.map.with_index do |asset, index|
+					source = asset['source']
+					
+					if source
+						type = source['type']
+						
+						case type
+						when 'github'
+							Sources::GithubSource.new(source)
+						else
+							Mmi.fail! %Q{Invalid "source.type" in asset #{index.inspect}: #{type.inspect}}
+						end
+					else
+						Mmi.fail! %Q{Missing "source" in asset #{index.inspect}.}
+					end
+				end
 			else
 				Mmi.fail! %Q{Invalid "assets": expected Array or nothing, got #{self.assets.inspect}.}
 			end
 		end
 		
-		def install_asset(asset, index)
-			source = asset['source']
-			
-			if source
-				type = source['type']
-				
-				case type
-				when 'github'
-					Sources::GithubSource.new(source).install(self.profile_dir)
-				else
-					Mmi.fail! %Q{Invalid "source.type" in asset #{index.inspect}: #{type.inspect}}
-				end
-			else
-				Mmi.fail! %Q{Missing "source" in asset #{index.inspect}.}
-			end
-		end
-		
 		def install
-			assets.each_with_index do |asset, index|
-				install_asset(asset, index)
+			assets.each do |asset|
+				asset.install(self.profile_dir)
 			end
 		end
 	end
