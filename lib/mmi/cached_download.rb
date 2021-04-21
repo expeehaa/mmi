ff6bbd75aa80c006469c1ec6ea3d2aff0981b7eb
expeehaa <expeehaa@outlook.com>
2021-04-21 14:36:27 +0200

    Factor out getting and checking a download stream.

--- lib/mmi/cached_download.rb
+++ lib/mmi/cached_download.rb
@@ -1,20 +1,27 @@
 module Mmi
 	module CachedDownload
 		class << self;
+			def download(uri, sha512: nil)
+				URI.open(uri).tap do |stream|
+					if sha512
+						actual_hexdigest = Digest::SHA512.hexdigest(stream.read)
+						
+						if sha512 == actual_hexdigest
+							stream.seek(0)
+						else
+							Mmi.fail! "Expected download to have SHA512 sum #{expected_hexdigest.inspect} but received #{actual_hexdigest.inspect}."
+						end
+					end
+				end
+			end
+			
 			def download_cached(uri, download_path, sha512_uri: nil)
 				ensure_cache_dir_exists!
 				
 				expected_hexdigest = URI.open(sha512_uri).read
 				
 				if !File.exists?(download_path) || expected_hexdigest != Digest::SHA512.hexdigest(File.read(download_path))
-					stream           = URI.open(uri)
-					actual_hexdigest = Digest::SHA512.hexdigest(stream.read)
-					
-					if expected_hexdigest == actual_hexdigest
-						stream.seek(0)
-					else
-						Mmi.fail! "Expected download to have SHA512 sum #{expected_hexdigest.inspect} but received #{actual_hexdigest.inspect}."
-					end
+					stream = download(uri, sha512: expected_hexdigest)
 					
 					IO.copy_stream(stream, download_path)
 				else
