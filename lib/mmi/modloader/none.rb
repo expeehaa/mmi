5a638fe80973980c0b0666caed3df18f8fbb6da1
expeehaa <expeehaa@outlook.com>
2022-01-23 16:03:55 +0100

    Add comments justifying empty method bodies.

--- lib/mmi/modloader/none.rb
+++ lib/mmi/modloader/none.rb
@@ -2,9 +2,11 @@ module Mmi
 	module Modloader
 		class None
 			def initialize(options=nil)
+				# Not installing anything requires no configuration or setup.
 			end
 			
 			def install
+				# Nothing to do.
 			end
 		end
 	end
