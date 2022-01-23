2f04bd529008f4e95f5f2dd6a390a11282ccb019
expeehaa <expeehaa@outlook.com>
2022-01-23 15:55:46 +0100

    Improve test description.

--- spec/lib/mmi/version_spec.rb
+++ spec/lib/mmi/version_spec.rb
@@ -1,7 +1,5 @@
-RSpec.describe Mmi do
-	describe '::VERSION' do
-		it 'is set' do
-			expect(Mmi::VERSION).to eq '0.1.3'
-		end
+RSpec.describe Mmi::VERSION do
+	it 'is set' do
+		expect(Mmi::VERSION).to eq '0.1.3'
 	end
 end
