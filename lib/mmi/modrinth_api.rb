e56d4afea30e69dfeb04bd6ecf94ee0f2a058d2b
expeehaa <expeehaa@outlook.com>
2024-05-19 23:26:02 +0200

    Add support for query parameters to "Mmi::ModrinthApi.project_versions".

--- lib/mmi/modrinth_api.rb
+++ lib/mmi/modrinth_api.rb
@@ -6,8 +6,8 @@ module Mmi
 		BASE_URL = URI('https://api.modrinth.com/v2/')
 		
 		class << self
-			def project_versions(mod_slug)
-				JSON.parse((BASE_URL + "project/#{mod_slug}/version").open.read)
+			def project_versions(mod_slug, loader: nil, game_version: nil)
+				JSON.parse((BASE_URL + "project/#{mod_slug}/version?#{URI.encode_www_form(loaders: (%Q{["#{loader}"]} if loader), game_versions: (%Q{["#{game_version}"]} if game_version))}").open.read)
 			end
 		end
 	end
