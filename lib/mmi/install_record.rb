51b27e62cea62f5d68de246d9a49de84f20f3bc4
expeehaa <expeehaa@outlook.com>
2024-07-18 23:50:18 +0200

    Use previous install record to optimize a new installation and clean up old files.

--- lib/mmi/install_record.rb
+++ lib/mmi/install_record.rb
@@ -19,16 +19,30 @@ module Mmi
 		end
 		
 		def install(dir)
+			old_record = self.class.parse_record_file(dir) || {}
+			
 			@record.each do |relative_path, entry|
 				target_file = File.expand_path(relative_path, dir)
 				
-				Mmi.info "Downloading #{entry.url.inspect} into #{target_file.inspect}."
-				
-				begin
-					Mmi::InstallUtils.download_to_file(entry.url, target_file)
-				rescue OpenURI::HTTPError => e
-					Mmi.fail! "Error when requesting asset.\n#{e.inspect}"
+				if old_record[relative_path] != entry || (File.exist?(target_file) && !entry.content_hash&.match_file?(target_file))
+					Mmi.info "Downloading #{entry.url.inspect} into #{target_file.inspect}."
+					
+					begin
+						Mmi::InstallUtils.download_to_file(entry.url, target_file)
+					rescue OpenURI::HTTPError => e
+						Mmi.fail! "Error when requesting asset.\n#{e.inspect}"
+					end
+				else
+					Mmi.info "Skipping #{entry.url.inspect} because it is already present at #{target_file.inspect}."
 				end
+				
+				old_record.delete(relative_path)
+			end
+			
+			old_record.each do |relative_path, _|
+				target_file = File.expand_path(relative_path, dir)
+				
+				File.delete(target_file) if File.exist?(target_file)
 			end
 			
 			File.write(File.expand_path(RECORD_FILE, dir), to_yaml)
@@ -36,6 +50,23 @@ module Mmi
 		
 		def to_yaml
 			Psych.dump(@record)
+		end
+		
+		def self.parse_record_file(dir)
+			record_file = File.expand_path(RECORD_FILE, dir)
+			
+			if File.exist?(record_file)
+				parsed_record = Psych.unsafe_load_file(record_file)
+				
+				unless !parsed_record.is_a?(Hash) || parsed_record.keys.any?{|k| !k.is_a?(String)} && parsed_record.values.any?{|v| !v.is_a?(Record)}
+					parsed_record
+				else
+					Mmi.warn("Found invalid install record at #{record_file}. The file will be ignored.")
+					nil
+				end
+			else
+				nil
+			end
 		end
 	end
 end
